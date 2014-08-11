1652938a6c411399ed70272ab2e315e88cacfe13
flickoid <chris.oatley@gmail.com>
2014-08-11 17:01:12 +0100

    add fix! method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,7 @@
 require 'bike'
 
 describe Bike do
+
 	it "should not be broken after we create it" do
 		the_bike = Bike.new
 		expect(the_bike).not_to be_broken
@@ -10,6 +11,13 @@ describe Bike do
 		bike = Bike.new
 		bike.break!
 		expect(bike).to be_broken
+	end
+
+	it "should be able to get fixed" do
+		bike = Bike.new
+		bike.break!
+		bike.fix!
+		expect(bike).not_to be_broken
 	end
 	
 end
