7490f377413421fdca281a28d5bda7c70968e826
flickoid <chris.oatley@gmail.com>
2014-08-12 16:24:08 +0100

    Added broken_bikes method

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -4,12 +4,19 @@ class ContainerHolder; include BikeContainer; end
 
 describe BikeContainer do 
 	let(:bike) { Bike.new }
+	let(:broken_bike) { Bike.new.break! }
 	let(:empty_holder) { ContainerHolder.new }
 	let(:holder_with_bike) { ContainerHolder.new }
 	let(:full_holder) { ContainerHolder.new }
-	
+	let(:mixed_full_holder) { ContainerHolder.new }
+
 	before { holder_with_bike.dock(bike)}
 	before { 10.times { full_holder.dock(bike) } }
+	before do 
+		 5.times { mixed_full_holder.dock(broken_bike) }
+		 5.times { mixed_full_holder.dock(bike) }
+	end
+
 
 	context "An empty BikeContainer" do 
 		it "should know it's empty" do 
@@ -66,7 +73,22 @@ describe BikeContainer do
 		it "should not accept a bike" do
 			expect { full_holder.dock(bike) }.to raise_error(RuntimeError, "Station is full")
 		end
-
 	end
+	context "A full container with 5 broken and 5 fixed bikes" do
+		it "should return an array when broken is called" do 
+			expect(mixed_full_holder.broken_bikes).to be_instance_of(Array)  
+		end
+
+		it "should return an array of 5 bikes" do
+			expect(mixed_full_holder.broken_bikes.count).to eq 5
+		end
+		it "should return 5 broken bikes" do 
+			mixed_full_holder.broken_bikes.each do |bike|
+				expect(bike).to be_broken
+			end
+		end
+
+	end 
+
 
 end
